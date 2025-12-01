268b73717ffb123fea619d0fcaaf0e9fa601f571
MadhavArora1213 <madhavarora132005@gmail.com>
2025-12-01 16:05:59 +0530

    correct

--- frontend/src/pages/PressPacksPage.jsx
+++ frontend/src/pages/PressPacksPage.jsx
@@ -598,7 +598,7 @@ const PressPacksPage = () => {
             <>
               {/* Enhanced Grid View */}
               {viewMode === 'grid' && (
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 md:gap-6">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 gap-4 md:gap-6">
                   {sortedPressPacks.map((pack, index) => (
                     <motion.div
                       key={pack.id}
