8e4f866744915794fe4bcb864af36e5033b45d7d
MadhavArora1213 <madhavarora132005@gmail.com>
2026-01-17 13:42:44 +0530

    feat: add How It Works page to explain platform usage with steps and FAQs.

--- frontend/src/pages/HowItWorks.jsx
+++ frontend/src/pages/HowItWorks.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
 import UserHeader from '../components/common/UserHeader';
 import UserFooter from '../components/common/UserFooter';
